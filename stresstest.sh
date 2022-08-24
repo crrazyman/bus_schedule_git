d3515b3d660e26d14bb07a49d3a0cbf7fe9a1221
Constantin MANEA <dmanea@Dmanea-mac.local>
2022-08-24 14:18:01 +0300

    update

--- stresstest.sh
+++ stresstest.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
+# Used https://github.com/tsenart/vegeta
 
+is_number () {
+    re='^[0-9]+$'
+    if ! [[ $1 =~ $re ]] ; then
+       echo "Error: Argument is not a number" >&2;
+       exit 1
+    fi
+    return 0
+}
 
-echo "Set attacked URL [GUI|API]"
-read ATTACKED
+echo -e "Set attacked URL:"
+read URL
 
-if [ "$ATTACKED" = "GUI" ]; then
-	URL = "http://127.0.0.1:55001"
-elif [ "$ATTACKED" = "API" ]; then
-	URL = "http://127.0.0.1:55001/buses"
-else
-	echo "Only GUI or API are permitted"
-	exit 1
-fi
-
-echo "Set attack rate"
+echo -e "\nSet attack rate"
 read RATE
+is_number $RATE
 
-re='^[0-9]+$'
-if ! [[ $RATE =~ $re ]] ; then
-   echo "Error: Rate is not a number" >&2;
-   exit 1
-fi
-
-echo "Set attack duration (s)"
+echo -e "\nSet attack duration (s)"
 read DURATION
+is_number $DURATION
 
-echo "GET $URL" | vegeta attack -rate=$RATE -duration=$DURATIONs | vegeta report --type=text
+DURATION="${DURATION}s"
+
+echo -e "\nAttacking $URL...\n\n"
+
+echo "GET $URL" | vegeta attack -rate=$RATE -duration=$DURATION | vegeta report --type=text
 
